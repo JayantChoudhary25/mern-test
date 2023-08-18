9d57097930e848202b781cfb1917a4c729d9b1e0
Jay <jayantchoudhary271@gmail.com>
2023-08-18 13:31:34 +0530

    poxy changes

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'https://vercel-test-one-drab.vercel.app/',
+      target: 'https://vercel-test-one-drab.vercel.app',
       changeOrigin: true,
       
     })
