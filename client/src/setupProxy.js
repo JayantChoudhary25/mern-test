d7b7aa29cbf59baa4580b3eafec57a5a98321d21
Jay <jayantchoudhary271@gmail.com>
2023-08-18 13:26:11 +0530

    porxi change

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:5000/',
+      target: 'https://vercel-test-one-drab.vercel.app/',
       changeOrigin: true,
       
     })
